6335dea9b70d44b754bad740c88bf14cbeee54e2
Luc Perkins <lucperkins@gmail.com>
2023-03-13 18:53:23 +0200

    Move build logic into example.nu script

--- flake.nix
+++ flake.nix
@@ -77,22 +77,7 @@
             nushell = pkgs.nushell;
             packages = with pkgs; [ go ];
             src = ./.;
-            build = ''
-              let share = $"($env.out)/share"
-              mkdir $share
-
-              let versionFile = $"($share)/go-version.txt"
-              echo $"Writing version info to ($versionFile)"
-              go version | save $versionFile
-
-              let helpFile = $"($share)/go-help.txt"
-              echo $"Writing help info to ($helpFile)"
-              go help | save $helpFile
-
-              [$versionFile $helpFile] | each {|f|
-                substituteInPlace $f --replace "go" --with "golang"
-              }
-            '';
+            build = ./example.nu;
           };
 
         # Derivation that relies on the Nushell derivation
