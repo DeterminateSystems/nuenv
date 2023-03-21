eac9f06a9c5a8e86c6876370db83c50dfb6948c3
Luc Perkins <lucperkins@gmail.com>
2023-03-21 13:10:00 +0200

    Fix misc issues in package outputs

--- flake.nix
+++ flake.nix
@@ -123,81 +123,82 @@
         };
       });
 
-      packages = forAllSystems
-        ({ pkgs, system }: rec {
-          default = hello;
+      packages = forAllSystems ({ pkgs, system }: rec {
+        default = hello;
 
-          # An example Nushell-based derivation
-          hello = pkgs.nuenv.mkDerivation {
-            name = "hello-nix-nushell";
-            packages = [ pkgs.hello ];
-            src = ./.;
-            build = builtins.readFile ./example/hello.nu;
-            MESSAGE = "Hello from Nix + Bash";
+        # An example Nushell-based derivation
+        hello = pkgs.nuenv.mkDerivation {
+          name = "hello-nix-nushell";
+          packages = [ pkgs.hello ];
+          src = ./.;
+          build = builtins.readFile ./example/hello.nu;
+          MESSAGE = "Hello from Nix + Bash";
+        };
+
+        # A non-overlay version
+        direct = self.lib.mkNushellDerivation pkgs.nushell system {
+          name = "no-overlay";
+          src = ./.;
+          build = ''
+            let share = $"($env.out)/share"
+            (version).version | save nushell-version.txt
+            mkdir $share
+            cp nushell-version.txt $share
+          '';
+        };
+
+        # The Nushell-based derivation above but with debug mode disabled
+        helloNoDebug = pkgs.nuenv.mkDerivation {
+          name = "hello-nix-nushell";
+          packages = [ pkgs.hello ];
+          src = ./.;
+          build = builtins.readFile ./example/hello.nu;
+          debug = false;
+          MESSAGE = "Hello from Nix + Bash";
+        };
+
+        # Show that Nuenv works when drawing sources from GitHub
+        githubSrc = pkgs.nuenv.mkDerivation {
+          name = "advanced-nix-nushell";
+          src = pkgs.fetchFromGitHub {
+            owner = "DeterminateSystems";
+            repo = "nuenv";
+            rev = "c8adf22d9cdc61d4777ad4b9d193e9b22547419e";
+            sha256 = "sha256-fu4WTFHlceasWpi6zF0nlent4KSmPAdsiKTrGixDEiI=";
           };
+          build = ''
+            let share = $"($env.out)/share"
+            mkdir $share
+            cp README.md $share
+          '';
+        };
 
-          # A non-overlay version
-          direct = self.lib.mkNushellDerivation pkgs.nushell system {
-            name = "no-overlay";
-            src = ./.;
-            build = ''
-              (version).version | save nushell-version.txt
-              cp nushell-version.txt $env.out
-            '';
-          };
+        # The same derivation above but using the stdenv
+        stdenv = pkgs.stdenv.mkDerivation {
+          name = "just-experimenting";
+          buildInputs = [ pkgs.hello ];
+          src = ./.;
+          buildPhase = ''
+            hello --greeting "''${MESSAGE}" > hello.txt
+            substituteInPlace hello.txt --replace "Bash" "Nushell"
+          '';
+          installPhase = ''
+            mkdir -p $out/share
+            cp hello.txt $out/share
+          '';
+          MESSAGE = "Hello from Nix + Bash";
+        };
 
-          # The Nushell-based derivation above but with debug mode disabled
-          helloNoDebug = pkgs.nuenv.mkDerivation {
-            name = "hello-nix-nushell";
-            packages = with pkgs; [ hello ];
-            src = ./.;
-            build = builtins.readFile ./example/hello.nu;
-            debug = false;
-            MESSAGE = "Hello from Nix + Bash";
-          };
+        # Simple that relies on the Nushell derivation
+        other = pkgs.stdenv.mkDerivation {
+          name = "other";
+          src = ./.;
+          installPhase = ''
+            mkdir -p $out/share
 
-          # Show that Nuenv works when drawing sources from GitHub
-          githubSrc = pkgs.nuenv.mkDerivation {
-            name = "advanced-nix-nushell";
-            src = pkgs.fetchFromGitHub {
-              owner = "DeterminateSystems";
-              repo = "nuenv";
-              rev = "c8adf22d9cdc61d4777ad4b9d193e9b22547419e";
-              sha256 = "sha256-fu4WTFHlceasWpi6zF0nlent4KSmPAdsiKTrGixDEiI=";
-            };
-            build = ''
-              let share = $"($env.out)/share"
-              mkdir $share
-              cp README.md $share
-            '';
-          };
-
-          # The same derivation above but using the stdenv
-          stdenv = pkgs.stdenv.mkDerivation {
-            name = "just-experimenting";
-            buildInputs = with pkgs; [ hello ];
-            src = ./.;
-            buildPhase = ''
-              hello --greeting "''${MESSAGE}" > hello.txt
-              substituteInPlace hello.txt --replace "Bash" "Nushell"
-            '';
-            installPhase = ''
-              mkdir -p $out/share
-              cp hello.txt $out/share
-            '';
-            MESSAGE = "Hello from Nix + Bash";
-          };
-
-          # Simple that relies on the Nushell derivation
-          other = pkgs.stdenv.mkDerivation {
-            name = "other";
-            src = ./.;
-            installPhase = ''
-              mkdir -p $out/share
-
-              cp ${self.packages.${system}.default}/share/hello.txt $out/share/copied.txt
-            '';
-          };
-        });
+            cp ${self.packages.${system}.hello}/share/hello.txt $out/share/copied.txt
+          '';
+        };
+      });
     };
 }
