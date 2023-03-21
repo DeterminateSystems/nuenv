a94f762fdb1ccfd1073b92857df079db32da5247
Luc Perkins <lucperkins@gmail.com>
2023-03-21 21:00:22 +0200

    Update hello message in Wasm app

--- rust-wasm-example/src/main.rs
+++ rust-wasm-example/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Welcome from Wasm built with Nix, Rust, and Nushell!");
+    println!(
+        "Hello 👋!
+This message is coming to you from a Wasi-compatible WebAssembly binary 🕸️
+It was written in Rust 🦀 and built by Nix ❄️
+But with a catch, as this isn't the usual Bash-based standard environment
+Oh no, this is a brand new environment based on Nushell!"
+    );
 }
